336a49938c507f73b0adff9ec2bfbd40dc612c85
Arnaud Clère <arnaud.clere@moulinarso.fr>
2022-06-24 19:00:14 +0200

    Fix hyperlinks Rename LICENSE > *.md to open in Win11 notepad

--- Source/App.xaml.cs
+++ Source/App.xaml.cs
@@ -1,8 +1,20 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
 
 namespace HandOnMouse
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            EventManager.RegisterClassHandler(
+                typeof(Hyperlink),
+                Hyperlink.RequestNavigateEvent,
+                new RequestNavigateEventHandler((sender, en) => Process.Start(new ProcessStartInfo(en.Uri.ToString()) { UseShellExecute = true }))
+            );
+            base.OnStartup(e);
+        }
     }
 }
